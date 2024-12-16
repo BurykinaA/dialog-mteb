c0340266031f89f0f3ba5371358414a833c4182c
Alina Burykina <alina4268@mail.ru>
2024-12-16 17:49:16 +0000

    fix

---
+++ scripts/two_public_datasets.sh
@@ -0,0 +1,23 @@
+export WORK_SPACE="/home/coder/project"
+export OUTPUT_DIR="/home/coder/project/dse/data/downstream_data"
+
+
+echo "1. Install ParlAi Package"
+
+cd $WORK_SPACE
+git clone https://github.com/facebookresearch/ParlAI.git 
+cd ParlAI; python setup.py develop
+
+
+echo "2. Use ParlAi to generate raw data"
+
+cd $WORK_SPACE/dse/data
+parlai convert_to_parlai --task amazon_qa --datatype train --outfile amazonqa.txt
+parlai convert_to_parlai --task dstc7 --datatype test --outfile ubuntu_test.txt
+parlai convert_to_parlai --task dstc7 --datatype valid --outfile ubuntu_valid.txt
+
+
+# echo "3. Use our scripts to generate evaluation data"
+
+# python data/process_evaluate.py --output_dir $OUTPUT_DIR --task amazonqa
+# python data/process_evaluate.py --output_dir $OUTPUT_DIR --task ubuntu
