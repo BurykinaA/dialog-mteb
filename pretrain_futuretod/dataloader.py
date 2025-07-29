ff8293a7ed5aef46b61fd611ae2f13260200416e
Alina Burykina <alina4268@mail.ru>
2025-07-29 11:30:40 +0300

    data

--- pretrain_futuretod/dataloader.py
+++ pretrain_futuretod/dataloader.py
@@ -53,7 +53,7 @@ class FutureTODDataset(Dataset):
         
         future_text = "".join(future_subset_turns).strip()
 
-        full_text = context_text + future_text
+        full_text = context_text + " " + self.tokenizer.sep_token + " " + future_text
 
         context_inputs = self.tokenizer(context_text, max_length=self.max_len, padding='max_length', truncation=True, return_tensors="pt")
         
