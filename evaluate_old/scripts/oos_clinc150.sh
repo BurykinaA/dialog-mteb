845ef64791b5a66ed9c9e0612e31a10b53cb9b46
Alina Burykina <alina4268@mail.ru>
2025-08-20 13:53:09 +0300

    oos

--- evaluate_old/scripts/oos_clinc150.sh
+++ evaluate_old/scripts/oos_clinc150.sh
@@ -1,19 +1,19 @@
 export CUDA_VISIBLE_DEVICES=0
 
-# Set default values if no arguments provided
-MODEL_DIR=${1:-"AndrewZeng/futuretod-base-v1.0"}
-DATA_DIR=${2:-"/home/coder/project/data/_downstream_data"}
-OUTPUT_DIR=${3:-"down_stream_old/metrics_futuretod"}
+# MODEL_DIR='AndrewZeng/futuretod-base-v1.0'
+# DATA_DIR='/home/coder/project/data/_downstream_data'
+# OUTPUT_DIR='down_stream_old/metrics_futuretod'
 
-echo "Using MODEL_DIR: $MODEL_DIR"
-echo "Using DATA_DIR: $DATA_DIR"
-echo "Using OUTPUT_DIR: $OUTPUT_DIR"
+MODEL_DIR='jasper_model_1/checkpoint-epoch-48'
+DATA_DIR='/home/coder/project/data/_downstream_data'
+OUTPUT_DIR='down_stream_old/metrics_jasper1_48'
 
 # Example usage
 # chmod +x evaluate_old/scripts/oos_clinc150.sh
 # ./evaluate_old/scripts/oos_clinc150.sh "bert-base-uncased" "/path/to/your/data" "/path/to/output"
 
-# Out-of-scope detection on Clinc150
+
+# Out-of-scope detection on Clinc150 5
 for data_ratio in 1 5
 do
     python evaluate_old/run_finetune.py \
